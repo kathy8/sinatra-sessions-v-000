9920a93cffa37e895a5f46634f2aa4ae8be87925
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-27 20:53:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,8 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
-    if session[:foo] = 'hello'
+    session[:foo] = 'hello'
+    if session[:foo] == 'hello'
       redirect '/fetch'
     else
       "Session value has not been set!"
@@ -62,4 +63,4 @@ class App < Sinatra::Base
 
   get '/finish' do
     "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
-  
+end
